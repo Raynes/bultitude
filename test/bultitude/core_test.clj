c2f5afd169a9179fc86288f33587a1ac60725e19
Hugo Duncan <hugo@hugoduncan.org>
2012-02-12 09:59:13 -0500

    Add some simple test cases

--- test/bultitude/core_test.clj
+++ test/bultitude/core_test.clj
@@ -2,6 +2,19 @@
   (:use clojure.test
         bultitude.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest namespaces-on-classpath-test
+  (testing "find clojure.core"
+    (is (seq (filter
+              #(= 'clojure.core %)
+              (namespaces-on-classpath)))))
+  (testing "prefix"
+    (is (seq (filter
+              #(= 'clojure.core %)
+              (namespaces-on-classpath :prefix "clojure.core"))))
+    (is (every?
+         #(.startsWith (name %) "clojure.core")
+         (namespaces-on-classpath :prefix "clojure.core"))))
+  (testing "directory"
+    (is (=
+         #{'bultitude.core 'bultitude.core-test}
+         (set (namespaces-on-classpath :prefix "bultitude"))))))
