6b6eecd7d014dfd4f06fe95ca28bf8fd16348dc7
Anthony Grimes <i@raynes.me>
2012-02-12 16:35:39 -0600

    Inc version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bultitude "0.1.1"
+(defproject bultitude "0.1.2"
   :description "A library for find Clojure namespaces on the classpath."
   :dependencies [[clojure "1.2.1"]]
   :multi-deps {"1.3.0" [[org.clojure/clojure "1.3.0"]]
