6ae9b72bdd9c8ec776a273fbca0134a82442cb82
Hugo Duncan <hugo@hugoduncan.org>
2012-02-12 09:58:37 -0500

    Make project depend on clojure 1.2.1, rather than 1.3.0, and add lein-multi

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject bultitude "0.1.1"
   :description "A library for find Clojure namespaces on the classpath."
-  :dependencies [[clojure "1.3.0"]])
+  :dependencies [[clojure "1.2.1"]]
+  :multi-deps {"1.3.0" [[org.clojure/clojure "1.3.0"]]
+               "1.4.0" [[org.clojure/clojure "1.4.0-beta1"]]})
