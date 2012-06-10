d7625f9b4d8a726228a2e12737263aa835b81fac
Anthony Grimes <i@raynes.me>
2012-06-10 17:25:33 -0500

    Add travis file and update project.clj.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject bultitude "0.1.6"
   :min-lein-version "2.0.0"
   :description "A library for find Clojure namespaces on the classpath."
-  :dependencies [[org.clojure/clojure "1.2.1"]]
-  :profiles {:1.4.0 {:dependencies [[org.clojure/clojure "1.4.0-beta1"]]}
-             :1.3.0 {:dependencies [[org.clojure/clojure "1.3.0"]]}})
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :aliases {"test-all" ["with-profile" "dev,default:dev,1.3,default:dev,1.2,default" "test"]}
+  :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}})
