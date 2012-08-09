62707be6034ad8f340468996d01c6aa098369e21
John Gabriele <jmg3000@gmail.com>
2012-08-09 11:44:49 -0300

    add :url to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject bultitude "0.1.6"
   :min-lein-version "2.0.0"
   :description "A library for find Clojure namespaces on the classpath."
+  :url "https://github.com/Raynes/bultitude"
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :aliases {"test-all" ["with-profile" "dev,default:dev,1.3,default:dev,1.2,default" "test"]}
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
