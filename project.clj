2353b1f24cf359cd707015d7394e3fdec75c9363
Tobias Crawley <tcrawley@gmail.com>
2012-11-21 22:34:38 -0500

    Update to the dynapath 0.2.0 api.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :description "A library for find Clojure namespaces on the classpath."
   :url "https://github.com/Raynes/bultitude"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [dynapath "0.1.0"]]
+                 [dynapath "0.2.0"]]
   :aliases {"test-all" ["with-profile" "dev,default:dev,1.3,default:dev,1.2,default" "test"]}
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}})
