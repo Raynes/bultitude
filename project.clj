a91cad3d7a79972bf6c22d16f124740e4a71892e
Anthony Grimes <i@raynes.me>
2014-01-16 21:12:44 -0800

    Version 0.2.4.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bultitude "0.2.3"
+(defproject bultitude "0.2.4"
   :min-lein-version "2.0.0"
   :description "A library for find Clojure namespaces on the classpath."
   :url "https://github.com/Raynes/bultitude"
