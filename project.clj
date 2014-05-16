96ae47afdd233d0e85b97190966efec0498b04fb
John Wiseman <jjwiseman@gmail.com>
2014-05-16 14:40:27 -0700

    Added :license to project.clj.
    This makes it easier to audit licenses with tools like lein-licenses.

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :min-lein-version "2.0.0"
   :description "A library for find Clojure namespaces on the classpath."
   :url "https://github.com/Raynes/bultitude"
+  :license {:name "Eclipse Public License 1.0"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.tcrawley/dynapath "0.2.3"]]
   :aliases {"test-all" ["with-profile" "dev,default:dev,1.3,default:dev,1.2,default" "test"]}
