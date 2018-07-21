298b4c7232bfd4fd9e66c80c43eb2842f11ebd6c
Timo Freiberg <timo.freiberg@gmail.com>
2018-07-21 14:53:12 +0200

    Set clojure version to 1.9
    Add test profiles for 7 and 8

--- project.clj
+++ project.clj
@@ -3,10 +3,12 @@
   :description "A library for find Clojure namespaces on the classpath."
   :url "https://github.com/timofreiberg/bultitude"
   :license {:name "Eclipse Public License 1.0"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.tcrawley/dynapath "1.0.0"]]
-  :aliases {"test-all" ["with-profile" "dev,default:dev,1.6:dev,1.5:dev,1.4:dev,1.3,dev" "test"]}
+  :aliases {"test-all" ["with-profile" "dev,default:dev,1.8:dev,1.7:dev,1.6:dev,1.5:dev,1.4:dev,1.3,dev" "test"]}
   :profiles {:test {:resources ["test-resources"]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
