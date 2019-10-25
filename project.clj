1beb7ebc97df989cbd3437e151b6eb6e6cc5d0d1
Timo Freiberg <timo.freiberg@novatec-gmbh.de>
2019-10-25 09:32:09 +0200

    Use clojure 10 by default

--- project.clj
+++ project.clj
@@ -2,10 +2,11 @@
   :description "A library for finding Clojure namespaces on the classpath."
   :url "https://github.com/timofreiberg/bultitude"
   :license {:name "Eclipse Public License 1.0"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.tcrawley/dynapath "1.0.0"]]
-  :aliases {"test-all" ["with-profile" "dev,default:dev,1.8:dev,1.7:dev,1.6:dev,1.5:dev,1.4:dev,1.3,dev" "test"]}
+  :aliases {"test-all" ["with-profile" "dev,default:dev,1.9:dev,1.8:dev,1.7:dev,1.6:dev,1.5:dev,1.4:dev,1.3,dev" "test"]}
   :profiles {:test {:resources ["test-resources"]}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
